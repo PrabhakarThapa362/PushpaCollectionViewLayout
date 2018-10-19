4863c1b1c22ca1bf9847845c38fdac26711d0a94
satish <satish.v@canopas.com>
2018-10-19 16:17:41 +0530

    add layout file

---
+++ PushpaCollectionViewLayout.swift
@@ -0,0 +1,100 @@
+//
+//  PushpaCollectionViewLayout.swift
+//  PushpaCollectionViewLayout
+//
+//  Created by Canopas on 19/10/18.
+//  Copyright © 2018 Canopas Inc. All rights reserved.
+//
+
+import UIKit
+
+class PushpaCollectionViewLayout: UICollectionViewLayout {
+    
+    public var itemSize: CGSize = CGSize(width: 280, height: 300) { didSet{ invalidateLayout() } }
+    public var maximumVisibleItems: Int = 4 { didSet{ invalidateLayout() } }
+    public var spacing:CGFloat = 10 { didSet { invalidateLayout() } }
+    public var leftSpace:CGFloat = 12 { didSet { invalidateLayout() } }
+    
+    override open var collectionView: UICollectionView {
+        return super.collectionView!
+    }
+    
+    override open var collectionViewContentSize: CGSize {
+        let itemsCount = CGFloat(collectionView.numberOfItems(inSection: 0))
+        return CGSize(width: collectionView.bounds.width * itemsCount,
+                      height: collectionView.bounds.height)
+    }
+    
+    override open func prepare() {
+        super.prepare()
+        assert(collectionView.numberOfSections == 1, "Multiple sections aren't supported!")
+    }
+    
+    override open func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+    
+    fileprivate var contentOffset:CGPoint   { return collectionView.contentOffset }
+    fileprivate var bounds:CGRect           { return collectionView.bounds }
+    fileprivate var totalCount:Int          { return collectionView.numberOfItems(inSection: 0) }
+    fileprivate var minIndex:Int            { return max(Int(contentOffset.x) / Int(bounds.width), 0) }
+    fileprivate var maxIndex:Int            { return min(minIndex + maximumVisibleItems, totalCount) }
+    fileprivate var deltaOffset:CGFloat     { return CGFloat(Int(collectionView.contentOffset.x) % Int(collectionView.bounds.width)) }
+    fileprivate var percentDelta:CGFloat    { return CGFloat(deltaOffset) / bounds.width }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var attributes:[UICollectionViewLayoutAttributes] = stride(from: minIndex, to: maxIndex, by: 1).map({ index in
+            let path = IndexPath.init(item: index, section: 0)
+            return self.getAttribute(for: path)
+        })
+        
+        if minIndex > 0 {
+            let previsItemAttr = IndexPath(item: minIndex - 1, section: 0)
+            attributes.append(self.getAttribute(for: previsItemAttr))
+        }
+        
+        return attributes
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        return getAttribute(for: indexPath)
+    }
+    
+    fileprivate func getAttribute(for indexPath:IndexPath) -> UICollectionViewLayoutAttributes {
+        let spacing:CGFloat = 10
+        let leftSpace:CGFloat = 12
+        let firstVisibleIndex = indexPath.row - minIndex
+        let attr = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+        attr.center.x = self.contentOffset.x + bounds.width / 2 + spacing * CGFloat(firstVisibleIndex)
+        attr.center.y = bounds.midY
+        attr.size = itemSize
+        attr.zIndex = maximumVisibleItems - firstVisibleIndex
+        
+        switch firstVisibleIndex {
+        case -1:
+            print("Previous index")
+            attr.center.x -= deltaOffset + bounds.width/2 + itemSize.width/2 - leftSpace - spacing
+        case 0:
+            let d = ensureRange(value: deltaOffset, minimum: -bounds.width, maximum: bounds.width/2 + itemSize.width/2 - leftSpace)
+            print("new delta",d)
+            attr.center.x -= d
+        case 1..<maximumVisibleItems:
+            attr.center.x -= spacing * percentDelta
+            
+            if firstVisibleIndex == maximumVisibleItems - 1 {
+                attr.alpha = percentDelta
+            }
+            break
+        default:
+            attr.alpha = 0
+            break
+        }
+        
+        return attr
+    }
+}
+
+//MARK: Helper
+fileprivate func ensureRange<T>(value: T, minimum: T, maximum: T) -> T where T : Comparable {
+    return min(max(value, minimum), maximum)
+}
--- PushpaCollectionViewLayout/ViewController.swift
+++ PushpaCollectionViewLayout/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var colors: [UIColor]  = [
+        UIColor(red: 137, green: 50, blue: 90),
+        UIColor(red: 149, green: 120, blue: 92),
+        UIColor(red: 194, green: 134, blue: 89),
+        UIColor(red: 1, green: 25, blue: 54),
+        UIColor(red: 155, green: 84, blue: 109)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.isPagingEnabled = true
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let layout = collectionView.collectionViewLayout as! PushpaCollectionViewLayout
+        layout.itemSize = CGSize(width: collectionView.frame.width - 100, height: collectionView.frame.width + 100)
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.item]
+        cell.layer.cornerRadius = 12
+        cell.layer.masksToBounds = true
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        self.init(red: CGFloat(red)/255 ,
+                  green: CGFloat(green)/255,
+                  blue: CGFloat(blue)/255,
+                  alpha: 1.0)
+    }
+}
